a6237ef36f260b3b1a696d4286a38c47ec126594
Javlon1 <muhamedmijotot@gmail.com>
2023-11-27 22:00:35 +0300

    start project

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+
+function openModal() {
+    document.getElementById('pop-up').style.display = 'flex';
+    document.getElementById('pop-up-close').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('pop-up').style.display = 'none';
+    document.getElementById('pop-up-close').style.display = 'none';
+    document.body.style.overflow = 'visible'; // or 'auto'
+}
+
+const btn = document.querySelector(".container__list__item__btn")
+btn.addEventListener("click", () => {
+    openModal()
+    document.body.style.overflow = 'hidden';
+})
